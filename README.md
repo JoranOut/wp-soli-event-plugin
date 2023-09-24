1c76ab5f047c688e483454fb96dac1b0b7635333
JoranOut <joranlol@gmail.com>
2023-09-24 16:20:41 +0200

    [plugin-updater] change updater config

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-!\[npm\](https://img.shields.io/badge/npm-v9.5.0-f39f37)
-!\[node\](https://img.shields.io/badge/node-v16.13.0-f39f37)
-!\[wp_env\](https://img.shields.io/badge/wp_env-v5.12.0-f39f37)
-!\[wordpress\](https://img.shields.io/badge/wordpress-v6.3.1-f39f37)
+![npm](https://img.shields.io/badge/npm-v9.5.0-fb8817)
+![node](https://img.shields.io/badge/node-v16.13.0-43853d)
+![wp_env](https://img.shields.io/badge/wp&dash;env-v5.12.0-40a8af)
+![wordpress](https://img.shields.io/badge/Wordpress-v6.3.1-3858e9)
 
 # WP Soli event plugin
 Plugin for wordpress dedicated to displaying events on [soli.nl](https://www.soli.nl)
--- soli-event-plugin.php
+++ soli-event-plugin.php
@@ -32,19 +32,17 @@ function github_plugin_updater_init() {
     $config = array(
       'slug' => plugin_basename( __FILE__ ), // this is the slug of your plugin
       'proper_folder_name' => plugin_basename( __FILE__ ), // this is the name of the folder your plugin lives in
-      'api_url' => 'https://api.github.com/repos/jkudish/WordPress-GitHub-Plugin-Updater', // the GitHub API url of your GitHub repo
-      'raw_url' => 'https://raw.github.com/jkudish/WordPress-GitHub-Plugin-Updater/master', // the GitHub raw url of your GitHub repo
-      'github_url' => 'https://github.com/jkudish/WordPress-GitHub-Plugin-Updater', // the GitHub url of your GitHub repo
-      'zip_url' => 'https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/archive/master.zip', // the zip url of the GitHub repo
+      'api_url' => 'https://api.github.com/repos/JoranOut/wp-soli-event-plugin', // the GitHub API url of your GitHub repo
+      'raw_url' => 'https://raw.github.com/JoranOut/wp-soli-event-plugin/master', // the GitHub raw url of your GitHub repo
+      'github_url' => 'https://github.com/JoranOut/wp-soli-event-plugin', // the GitHub url of your GitHub repo
+      'zip_url' => 'https://github.com/JoranOut/wp-soli-event-plugin/archive/master.zip', // the zip url of the GitHub repo
       'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-      'requires' => '3.0', // which version of WordPress does your plugin require?
-      'tested' => '3.3',  // which version of WordPress is your plugin tested up to?
+      'requires' => '6.0.0', // which version of WordPress does your plugin require?
+      'tested' => '6.3.1',  // which version of WordPress is your plugin tested up to?
       'readme' => 'README.md', // which file to use as the readme for the version number
-      'access_token' => '', // Access private repositories by authorizing under Plugins > GitHub Updates when this example plugin is installed
     );
 
     new WP_GitHub_Updater( $config );
-
   }
 
 }
