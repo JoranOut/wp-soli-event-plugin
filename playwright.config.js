5d83b115e5950976851a7aac35a3010155120af5
JoranOut <jorankorverout@digiboox.app>
2025-09-21 21:18:43 +0200

    [e2e] Add video config

--- playwright.config.js
+++ playwright.config.js
@@ -2,8 +2,14 @@ import {defineConfig} from '@playwright/test';
 
 export default defineConfig({
     testDir: 'e2e',
+    retries: process.env.CI ? 1 : 0,                 // enables "on-first-retry" if you prefer it
+    reporter: [['html', { open: 'never' }]],
     use: {
-        baseURL: 'http://localhost:8889'
+        baseURL: process.env.BASE_URL || 'http://localhost:8889',
+        screenshot: 'only-on-failure',
+        video: process.env.CI ? 'retain-on-failure' : 'on', // keep videos for failures in CI
+        trace: 'retain-on-failure',                         // full click-by-click trace on failure
     },
+    outputDir: 'test-results',                            // where videos/traces/screens land
     webServer: undefined
 });
